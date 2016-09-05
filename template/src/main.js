372e440a46b8bf2155f76d43df624f11cdcbfea1
MeiLin <postget.me@gmail.com>
2016-09-05 21:52:24 +0800

    update

--- template/src/main.js
+++ template/src/main.js
@@ -1,3 +1,8 @@
+require('expose?$!expose?jQuery!jquery')
+require('bootstrap')
+require('font-awesome/css/font-awesome.min.css')
+require('bootstrap/dist/css/bootstrap.css')
+
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
